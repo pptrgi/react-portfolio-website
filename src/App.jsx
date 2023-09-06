7a8c727ee1d482312d1924e60bd66f1f9bc28177
pptrgi <pptrgitonga@gmail.com>
2023-09-06 19:20:55 +0300

    Add the 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,58 @@
-import {createBrowserRouter, Outlet, ScrollRestoration, RouterProvider} from 'react-router-dom'
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Home from './pages/Home';
-import { serveProjects } from './components/projects/ProjectsData';
-import ProjectDetailPage from './components/projects/ProjectDetailPage';
-import Resume from './pages/Resume';
-import './App.css';
-
+import {
+  createBrowserRouter,
+  Outlet,
+  ScrollRestoration,
+  RouterProvider,
+} from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import { serveProjects } from "./components/projects/ProjectsData";
+import ProjectDetailPage from "./components/projects/ProjectDetailPage";
+import Resume from "./pages/Resume";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-    const Layout = () => {
-        return (
-            <div>
-                <Header />
-                <ScrollRestoration />
-                <Outlet />
-                <Footer />
-            </div>
-        )
-    }
-    const router = createBrowserRouter([{
-        path: '/',
-        element: <Layout />,
-        children: [
-            {
-                path: '/',
-                element: <Home />,
-                loader: serveProjects
-            }, 
-            {
-                path: '/project/:id',
-                element: <ProjectDetailPage />
-            },
-            {
-                path:'/cv',
-                element: <Resume />
-            }
-        ]
-    }])
+  const Layout = () => {
+    return (
+      <div>
+        <Header />
+        <ScrollRestoration />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+          loader: serveProjects,
+        },
+        {
+          path: "/project/:id",
+          element: <ProjectDetailPage />,
+        },
+        {
+          path: "/cv",
+          element: <Resume />,
+        },
+        {
+          path: "*",
+          element: <NotFoundPage />,
+        },
+      ],
+    },
+  ]);
   return (
     <div>
-        <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
