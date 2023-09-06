96193fadc14bcdc9e7169ade56733a17609c3630
pptrgi <pptrgitonga@gmail.com>
2023-09-06 19:18:23 +0300

    Remove container padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       colors: {
@@ -16,17 +13,16 @@ export default {
         extraWhite: "#f7f7f7",
         extraPurple: "#5267DF",
         containerColor: "#fff",
-      }
+      },
     },
     container: {
       center: true,
-      padding: "1rem",
       screens: {
-        sm: '350px',
-        md: '576px',
-        lg: '768px',
-        iPad: '834px',
-        xl: '992px',
+        sm: "350px",
+        md: "576px",
+        lg: "768px",
+        iPad: "834px",
+        xl: "992px",
         "2xl": "1440px",
       },
     },
@@ -38,14 +34,13 @@ export default {
       normalFontSize: "1rem",
       smallFontSize: "0.875rem",
       smallerFontSize: "0.813rem",
-      tinyFontSize: "0.625rem"
+      tinyFontSize: "0.625rem",
     },
     fontWeight: {
       normalWeight: "400",
       mediumWeight: "500",
-      semiBolded: "600"
-    }
+      semiBolded: "600",
+    },
   },
   plugins: [],
-}
-
+};
