8761d66b6f98f3c11a58730b49270979abd291b4
pptrgi <pptrgitonga@gmail.com>
2023-11-30 18:00:46 +0300

    Add link to new Next portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+
 import { useLoaderData } from "react-router-dom";
 import LandingPage from "../components/landingPage/LandingPage";
 import Skills from "../components/skills/Skills";
@@ -6,6 +7,7 @@ import Services from "../components/services/Services";
 import Projects from "../components/projects/Projects";
 import Contact from "../components/contact/Contact";
 import About from "../components/about/About";
+import NewPortfolioPopup from "../components/NewPortfolioPopup";
 
 function Home() {
   const [projects, setProjects] = useState([]);
@@ -16,6 +18,9 @@ function Home() {
 
   return (
     <div className="">
+      <div className="sm:relative sm:container">
+        <NewPortfolioPopup />
+      </div>
       <LandingPage />
       <About />
       <Skills />
